c453e53cedac47f1fd53a7c2091f06b33d2fed5f
simonazy <56880104+simonazy@users.noreply.github.com>
2021-09-28 17:11:21 -0400

    Elevator System

---
+++ Elevator_System/cmake-build-debug/CMakeFiles/elev_sys_sim.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/bravery_simon/Documents/UdacityCPP-master/Elevator_System/src/elevator.cpp" "/Users/bravery_simon/Documents/UdacityCPP-master/Elevator_System/cmake-build-debug/CMakeFiles/elev_sys_sim.dir/src/elevator.cpp.o"
+  "/Users/bravery_simon/Documents/UdacityCPP-master/Elevator_System/src/elevator_sim.cpp" "/Users/bravery_simon/Documents/UdacityCPP-master/Elevator_System/cmake-build-debug/CMakeFiles/elev_sys_sim.dir/src/elevator_sim.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Elevator_System/cmake-build-debug/CMakeFiles/elev_sys_sim.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/elev_sys_sim.dir/src/elevator.cpp.o"
+  "CMakeFiles/elev_sys_sim.dir/src/elevator_sim.cpp.o"
+  "elev_sys_sim"
+  "elev_sys_sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/elev_sys_sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
